fc54e406b98f300054f28a1e64bb08228e9c2cbc
Kul Deep Varma <kuldeepvarma7413@gmail.com>
2024-06-20 20:14:22 +0530

    userrouter fixes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,19 +4,18 @@ const User = require("../models/user.model");
 
 // get user by email and account type
 router.get("/", async (req, res) => {
-  // const { email, accountType } = req.query;
-  // try {
-  //   console.log(email, accountType);
-  //   const user = await User.findOne({ email: email, accountType: accountType });
-  //   if (!user) {
-  //     return res.json({ status: "ERROR", message: "User not found" });
-  //   }
-  //   return res.json({ status: "OK", user });
-  // } catch (err) {
-  //   console.error(err);
-  //   return res.json({ status: "ERROR", message: "Server error" });
-  // }
-  return res.json({message: "User Router"});
+  const { email, accountType } = req.query;
+  try {
+    console.log(email, accountType);
+    const user = await User.findOne({ email: email, accountType: accountType });
+    if (!user) {
+      return res.json({ status: "ERROR", message: "User not found" });
+    }
+    return res.json({ status: "OK", user });
+  } catch (err) {
+    console.error(err);
+    return res.json({ status: "ERROR", message: "Server error" });
+  }
 });
 
 module.exports = router;
